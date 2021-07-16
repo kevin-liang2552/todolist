4cde1563e75cdfdb2cdb58a3069512e8ac8d5f23
Kevin Liang <kevin.liang@siteminder.com>
2021-07-16 16:03:33 +1000

    fix express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 import {testingConnection} from './models/database'
+import express from 'express';
+
+const app = express();
+const port = 8080;
 
 testingConnection();
+
+app.listen(port, () => {
+  console.log(`Hello World! Running on port ${port}`);
+});
+
+app.get('/', (_req, res) => {
+    res.send('Well done!');
+})

82729707497f7c27a08f619a0940406b2a80be6f
Kevin Liang <kevin.liang@siteminder.com>
2021-07-27 09:29:54 +1000

    fix logic issue

--- src/server/routes/tasks.routes.ts
+++ src/server/routes/tasks.routes.ts
@@ -2,19 +2,18 @@ import express from 'express';
 import * as _ from '../../controllers/get_tasks';
 
 const router = express.Router();
-const valid_status = 'complete'||'incomplete';
 
 router.get('/tasks', async (_req, res) => {
-    const tasks = _.get_all_tasks();
+    const tasks = await _.get_all_tasks();
     res.status(200).send(tasks);
 });
 
 router.get('/tasks/:status', async (req, res) => {
     const status = req.params.status;
-    if(status != valid_status){
-        res.status(400);
+    if(status !== 'complete' && status !== 'incomplete'){
+        res.status(400).send('Not a valid status');
     }else{
-        const tasks = _.get_tasks(status);
+        const tasks = await _.get_tasks(status);
         res.status(200).send(tasks);
     }
     

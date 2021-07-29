7fc1808195e2b90b7d0cf4b1bec1e8c20f82d54c
Kevin Liang <kevin.liang@siteminder.com>
2021-07-29 16:46:30 +1000

    added basic tests for service

--- src/services/tasks.service.test.ts
+++ src/services/tasks.service.test.ts
@@ -1,20 +1,23 @@
 import { taskService } from '.';
 import { Task } from '../models/task.model';
 import { sequelize } from './database';
+import { ETaskStatus } from '../models/share';
 import '../config';
 
 const testTasks = [
   {task: 'Buy eggs',
-  status: 'incomplete'},
+  status: ETaskStatus.Incomplete},
   {task: 'Buy milk',
-  status: 'complete'},
+  status: ETaskStatus.Complete},
   {task: 'Buy cheese',
-  status: 'incomplete'},
+  status: ETaskStatus.Incomplete},
   {task: 'Buy bacon',
-  status: 'incomplete'},
+  status: ETaskStatus.Incomplete},
 ];
 
-beforeAll(async ()=>{
+const newTask = 'Buy chocolate';
+
+beforeEach(async ()=>{
   await Task.sync({force: true})
   await Task.destroy({
       truncate: true
@@ -29,19 +32,19 @@ afterAll(()=>{
 describe('getTaskByStatus', ()=> {
 
   test('Should return only incomplete', async() => {        
-    const tasks = await taskService.getTaskByStatus('incomplete'); 
+    const tasks = await taskService.getTaskByStatus(ETaskStatus.Incomplete); 
     expect(tasks).toHaveLength(3)
     tasks.forEach((task)=>{
-      expect(task.get('status')).toBe('incomplete');
+      expect(task.status).toBe(ETaskStatus.Incomplete);
       expect(testTasks).toContainEqual({task: task.task, status: task.status});
     });
   });
 
   test('Should return only complete', async() => {
-    const tasks = await taskService.getTaskByStatus('complete'); 
+    const tasks = await taskService.getTaskByStatus(ETaskStatus.Complete); 
     expect(tasks).toHaveLength(1);
     tasks.forEach((task)=>{
-      expect(task.get('status')).toBe('complete');
+      expect(task.status).toBe(ETaskStatus.Complete);
       expect(testTasks).toContainEqual({task: task.task, status: task.status});
     });
   });
@@ -53,7 +56,97 @@ describe('getAllTasks', ()=> {
     const tasks = await taskService.getAllTasks(); 
     expect(tasks).toHaveLength(4);
     tasks.forEach((task)=>{
-      expect(testTasks).toContainEqual({task: task.get('task'), status: task.get('status')});
+      expect(testTasks).toContainEqual({task: task.task, status: task.status});
     });
   });
+});
+
+describe('getTaskByID', ()=> {
+
+  test('Should return a task of a specific task of an ID in the database', async() => {    
+    const newTaskID = await taskService.addTask(newTask); 
+    const data = await taskService.getTaskByID(newTaskID);
+
+    const foundTask = {id: data.id, task: data.task, status: data.status}
+
+    expect(foundTask).toStrictEqual({id: newTaskID, task:newTask, status: ETaskStatus.Incomplete});
+
+  });
+
+});
+
+describe('addTask', ()=> {
+
+  test('Should add a task to the database', async() => {    
+    const newTaskID = await taskService.addTask(newTask); 
+    const allTasks = await taskService.getAllTasks();
+
+    expect(allTasks.length).toBe(5);
+
+    const data = await taskService.getTaskByID(newTaskID);
+
+    const foundTask = {id: data.id, task: data.task, status: data.status}
+
+    expect(foundTask).toStrictEqual({id: newTaskID, task:newTask, status: ETaskStatus.Incomplete})
+
+  });
+
+});
+
+describe('deleteTaskByStatus', ()=> {
+
+  test('Should delete all complete tasks in the database', async() => {    
+    await taskService.deleteTaskByStatus(ETaskStatus.Complete);
+    const allTasks = await taskService.getAllTasks();
+
+    expect(allTasks.length).toBe(3);
+
+  });
+
+  test('Should delete all incomplete tasks in the database', async() => {    
+    await taskService.deleteTaskByStatus(ETaskStatus.Incomplete);
+    const allTasks = await taskService.getAllTasks();
+
+    expect(allTasks.length).toBe(1);
+
+  });
+
+});
+
+describe('deleteTaskByID', ()=> {
+
+  test('Should delete a specific task in the database', async() => {    
+    const newTaskID = await taskService.addTask(newTask); 
+    const allTasks = await taskService.getAllTasks();
+
+    expect(allTasks.length).toBe(5);
+
+    await taskService.deleteTaskByID(newTaskID);
+    const newAllTasks = await taskService.getAllTasks();
+
+    expect(newAllTasks.length).toBe(4);
+    newAllTasks.forEach((task)=>{
+      expect(testTasks).toContainEqual({task: task.task, status: task.status});
+    });
+  });
+
+});
+
+describe('updateTask', ()=> {
+
+  test('Should update the status of a task from "incomplete" to "complete"', async() => {    
+    const newTaskID = await taskService.addTask(newTask); 
+    const data = await taskService.getTaskByID(newTaskID);
+
+    const foundTask = {id: data.id, task: data.task, status: data.status};
+    expect(foundTask).toStrictEqual({id: newTaskID, task:newTask, status: ETaskStatus.Incomplete});
+
+    await taskService.updateTask(foundTask.id);
+    const newData = await taskService.getTaskByID(newTaskID);
+
+    const updatedTask = {id: newData.id, task: newData.task, status: newData.status};
+    expect(updatedTask).toStrictEqual({id: newTaskID, task:newTask, status: ETaskStatus.Complete});
+
+  });
+
 });

0a5dc88ae819f71eef0061e0935ce2be686261e9
Kevin Liang <kevin.liang@siteminder.com>
2021-07-29 10:12:52 +1000

    add tasks tests:

--- src/services/tasks.service.test.ts
+++ src/services/tasks.service.test.ts
@@ -1,72 +1,59 @@
-// Please update this test file
+import { taskService } from '.';
+import { Task } from '../models/task.model';
+import { sequelize } from './database';
 
-describe('task.service', () => {
-  it('should pass the test', () => {
-    expect(1).toEqual(1)
-  })
+const testTasks = [
+  {task: 'Buy eggs',
+  status: 'incomplete'},
+  {task: 'Buy milk',
+  status: 'complete'},
+  {task: 'Buy cheese',
+  status: 'incomplete'},
+  {task: 'Buy bacon',
+  status: 'incomplete'},
+];
+
+
+beforeAll(async ()=>{
+await Task.sync({force: true})
+await Task.destroy({
+    truncate: true
 })
+await Task.bulkCreate(testTasks);
+});
 
-// import * as _ from './get_tasks';
-// import database, {models} from '../database/index';
+afterAll(()=>{
+sequelize.close();
+});
 
-// const testTasks = [
-//     {task: 'Buy eggs',
-//     status: 'incomplete'},
-//     {task: 'Buy milk',
-//     status: 'complete'},
-//     {task: 'Buy cheese',
-//     status: 'incomplete'},
-//     {task: 'Buy bacon',
-//     status: 'incomplete'},
-// ];
+describe('getTaskByStatus', ()=> {
 
+  test('Should return only incomplete', async() => {        
+    const tasks = await taskService.getTaskByStatus('incomplete'); 
+    expect(tasks).toHaveLength(3)
+    tasks.forEach((task)=>{
+      expect(task.get('status')).toBe('incomplete');
+      expect(testTasks).toContainEqual({task: task.task, status: task.status});
+    });
+  });
 
-// beforeAll(async ()=>{
-//     await models.task.sync({force: true});
+  test('Should return only complete', async() => {
+    const tasks = await taskService.getTaskByStatus('complete'); 
+    expect(tasks).toHaveLength(1);
+    tasks.forEach((task)=>{
+      expect(task.get('status')).toBe('complete');
+      expect(testTasks).toContainEqual({task: task.task, status: task.status});
+    });
+  });
+});
 
-//     await models.task.destroy({
-//         truncate: true
-//     });
+describe('getAllTasks', ()=> {
 
-//     await models.task.bulkCreate(testTasks);
-// });
-
-// afterAll(()=>{
-//     database.close();
-// });
-
-// describe('get_tasks', ()=> {
-//     test('Should return only incomplete', async() => {        
-//         const tasks = await _.get_tasks('incomplete'); 
-//         expect(tasks).toHaveLength(3);
-
-//         tasks.forEach((task)=>{
-//             expect(task.get('status')).toBe('incomplete');
-//             expect(testTasks).toContainEqual({task: task.get('task'), status: task.get('status')});
-//         })
-//     });
-
-//     test('Should return only complete', async() => {
-
-//         const tasks = await _.get_tasks('complete'); 
-//         expect(tasks).toHaveLength(1);
-
-//         tasks.forEach((task)=>{
-//             expect(task.get('status')).toBe('complete');
-//             expect(testTasks).toContainEqual({task: task.get('task'), status: task.get('status')});
-//         })
-
-//     });
-// });
-
-// describe('get_all_tasks', ()=> {
-//     test('Should return all tasks in the database', async() => {    
-//         const tasks = await _.get_all_tasks(); 
-//         expect(tasks).toHaveLength(4);
-
-//         tasks.forEach((task)=>{
-//             expect(testTasks).toContainEqual({task: task.get('task'), status: task.get('status')});
-//         })
-
-//     });    
-// })
+  test('Should return all tasks in the database', async() => {    
+    const tasks = await taskService.getAllTasks(); 
+    expect(tasks).toHaveLength(4);
+    tasks.forEach((task)=>{
+      expect(testTasks).toContainEqual({task: task.get('task'), status: task.get('status')});
+    });
+  });
+});
